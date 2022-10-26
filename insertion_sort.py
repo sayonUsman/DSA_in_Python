1982da6bec76c6145434d72be8f64f7b7c7ab7bc
Sayon Usman <sayonusman.ma@gmail.com>
2022-10-26 14:57:57 +0300

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,15 @@
 class InsertionSort:
 	def __init__(self, list):
-		self.list = list
+		self.__list = list
 		
 	def insertion_sort(self):
-		for index in range(1, len(self.list)):
-			current = self.list[index]
+		for index in range(1, len(self.__list)):
+			current = self.__list[index]
 			index2 = index - 1
-			while index2 >= 0 and self.list[index2] > current:
-				self.list[index2 + 1] = self.list[index2]
+			while index2 >= 0 and self.__list[index2] > current:
+				self.__list[index2 + 1] = self.__list[index2]
 				index2 -= 1			
-			self.list[index2 + 1] = current 
+			self.__list[index2 + 1] = current 
 
 
 numbers = list(map(int, input("Please enter the list of number: ").split()))
