a72145bd58bf4a1e0d0fc005089e5b36562ecf7b
Sayon Usman <sayonusman.ma@gmail.com>
2022-10-26 14:52:50 +0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,17 +1,17 @@
 class BubbleSort:
 	def __init__(self, list):
-		self.list = list
+		self.__list = list
 		
 	def swap(self, index, index2):
-		temp = self.list[index]
-		self.list[index]= self.list[index2]
-		self.list[index2] = temp
+		temp = self.__list[index]
+		self.__list[index]= self.__list[index2]
+		self.__list[index2] = temp
 		
 	def bubble_sort(self):
-		for index in range(len(self.list) - 1):
+		for index in range(len(self.__list) - 1):
 			is_sorted = True
-			for index2 in range(1, len(self.list) - index):
-				if self.list[index2] < self.list[index2 - 1]:
+			for index2 in range(1, len(self.__list) - index):
+				if self.__list[index2] < self.__list[index2 - 1]:
 					self.swap(index2 - 1, index2)
 					is_sorted = False
 			if is_sorted:
